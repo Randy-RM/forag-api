37d9b5e5802c9963c304ccd80ce28fe7a5d5c14d
Randy-RM <randymuhema@gmail.com>
2021-11-02 16:03:39 +0100

    feat : add seeder for role and refactor index in models folder

--- src/models/index.js
+++ src/models/index.js
@@ -13,6 +13,15 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
 fs.readdirSync(__dirname)
   .filter((file) => {
     return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
---
+++ src/seeders/role.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert(
+      "Roles",
+      [
+        {
+          roleName: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          roleName: "organization",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          roleName: "user",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Roles", null, {});
+  },
+};

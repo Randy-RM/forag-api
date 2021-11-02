03c8cf49e4495c145f956637487d98874826f902
Randy-RM <randymuhema@gmail.com>
2021-11-02 16:14:01 +0100

    refactor : entry point file for app

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const authRouter = require('./routes/auth.routes');
+const userRouter = require('./routes/user.routes');
+const path = require('./config/paths');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+const corsOptions = {
+  origin: `http://localhost:${PORT}`,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
+  next();
+});
+
+//user router
+app.use(path.authBaseURI, authRouter);
+app.use(path.usersBaseURI, userRouter);
+
+app.listen(PORT, () => {
+  console.log(`The server listens on the port http://localhost:${PORT}`);
+});

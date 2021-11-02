79e78a147cfcb51ac5582d8e8057887aab65ce04
Randy-RM <randymuhema@gmail.com>
2021-11-02 15:52:32 +0100

    feat : add router for user

---
+++ src/routes/user.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { validateInputUser, authJwt } = require("../middlewares/index");
+const UserController = require("../controllers/userController");
+const userRouter = express.Router();
+
+userRouter.get("/all-activity", UserController.allActivity);
+
+userRouter.get(
+  "/profil/user",
+  [authJwt.verifyToken, authJwt.isUser],
+  UserController.userProfil
+);
+
+userRouter.get(
+  "/profil/organization",
+  [authJwt.verifyToken, authJwt.isOrganization],
+  UserController.organizationProfil
+);
+
+userRouter.get(
+  "/profil/admin",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  UserController.adminProfil
+);
+
+module.exports = userRouter;

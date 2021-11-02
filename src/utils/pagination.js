13737148902089f9ae1e22f53e6ed008a598187e
Randy-RM <randymuhema@gmail.com>
2021-11-02 16:07:10 +0100

    feat : add pagination functionality

---
+++ src/utils/pagination.js
@@ -0,0 +1,19 @@
+function getPagination(page, size) {
+  const limit = size ? +size : 3;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+}
+
+function getPagingData(data, page, limit) {
+  const { count: totalItems, rows: results } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, totalPages, currentPage, results };
+}
+
+module.exports = {
+  getPagination,
+  getPagingData,
+};
